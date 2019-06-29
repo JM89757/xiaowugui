6dafc8e89178069e443e3169c8ac7f39356f3b5f
张三 <zhangsan@itcast.cn>
2019-06-29 15:09:11 +0800

    brand project demo1

---
+++ pinyougou_root/pinyougou_parent/pinyougou_sellergoods_interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,30 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.entity.PageResult;
+
+import java.util.List;
+
+public interface BrandService {
+    public List<TbBrand> findAll();
+
+    public PageResult findPage(Integer pageNum, Integer pageSize);
+
+    public void add(TbBrand brand);
+
+    public void update(TbBrand brand);
+
+    public TbBrand findOne(Long id);
+
+    public void delete(Long[] ids);
+
+    /**
+     *
+     * @param brand 对象
+     * @param pageNum 当前页码
+     * @param pageSize 当前总条数
+     * @return 封装到结果集
+     */
+    public PageResult findPage(TbBrand brand, Integer pageNum, Integer pageSize);
+
+}
---
+++ pinyougou_root/pinyougou_parent/pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,75 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.common.utils.StringUtils;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.pojo.entity.PageResult;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    @Autowired
+    private TbBrandMapper brandMapper;
+
+
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null);
+    }
+
+    @Override
+    public PageResult findPage(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(null);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    @Override
+    public void add(TbBrand brand) {
+        brandMapper.insert(brand);
+    }
+
+    @Override
+    public void update(TbBrand brand) {
+        brandMapper.updateByPrimaryKey(brand);
+    }
+
+    @Override
+    public TbBrand findOne(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            brandMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+    @Override
+    public PageResult findPage(TbBrand brand, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        TbBrandExample tbBrandExample = new TbBrandExample();
+        TbBrandExample.Criteria criteria = tbBrandExample.createCriteria();
+
+        if (brand != null) {
+            if (!StringUtils.isEmpty(brand.getName())) {
+                criteria.andNameLike("%" + brand.getName() + "%");
+            }
+            if (!StringUtils.isEmpty(brand.getFirstChar())) {
+//                criteria.andFirstCharLike("%" + brand.getFirstChar() + "%");
+                criteria.andFirstCharEqualTo(brand.getFirstChar());
+            }
+        }
+        Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(tbBrandExample);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+}
