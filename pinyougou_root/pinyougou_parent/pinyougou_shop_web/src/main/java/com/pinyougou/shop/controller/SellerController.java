97bc2832888c066c911cf9c4597f6fe9e49cb167
张三 <zhangsan@itcast.cn>
2019-07-03 20:28:31 +0800

    brand project demo1 day5 2019年7月3日20:28:20

--- pinyougou_root/pinyougou_parent/pinyougou_shop_web/src/main/java/com/pinyougou/shop/controller/SellerController.java
+++ pinyougou_root/pinyougou_parent/pinyougou_shop_web/src/main/java/com/pinyougou/shop/controller/SellerController.java
@@ -2,6 +2,7 @@ package com.pinyougou.shop.controller;
 
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,11 @@ import com.pinyougou.sellergoods.service.SellerService;
 
 import entity.PageResult;
 import entity.Result;
+
 /**
  * controller
- * @author Administrator
  *
+ * @author Administrator
  */
 @RestController
 @RequestMapping("/seller")
@@ -25,30 +27,38 @@ public class SellerController {
 
     /**
      * 返回全部列表
+     *
      * @return
      */
     @RequestMapping("/findAll")
-    public List<TbSeller> findAll(){
+    public List<TbSeller> findAll() {
         return sellerService.findAll();
     }
 
 
     /**
      * 返回全部列表
+     *
      * @return
      */
     @RequestMapping("/findPage")
-    public PageResult  findPage(int page,int rows){
+    public PageResult findPage(int page, int rows) {
         return sellerService.findPage(page, rows);
     }
 
     /**
      * 增加
+     *
      * @param seller
      * @return
      */
     @RequestMapping("/add")
-    public Result add(@RequestBody TbSeller seller){
+    public Result add(@RequestBody TbSeller seller) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String password = bCryptPasswordEncoder.encode(seller.getPassword());
+        seller.setPassword(password);
+
+
         try {
             sellerService.add(seller);
             return new Result(true, "增加成功");
@@ -60,11 +70,12 @@ public class SellerController {
 
     /**
      * 修改
+     *
      * @param seller
      * @return
      */
     @RequestMapping("/update")
-    public Result update(@RequestBody TbSeller seller){
+    public Result update(@RequestBody TbSeller seller) {
         try {
             sellerService.update(seller);
             return new Result(true, "修改成功");
@@ -76,21 +87,23 @@ public class SellerController {
 
     /**
      * 获取实体
+     *
      * @param id
      * @return
      */
     @RequestMapping("/findOne")
-    public TbSeller findOne(String id){
+    public TbSeller findOne(String id) {
         return sellerService.findOne(id);
     }
 
     /**
      * 批量删除
+     *
      * @param ids
      * @return
      */
     @RequestMapping("/delete")
-    public Result delete(String [] ids){
+    public Result delete(String[] ids) {
         try {
             sellerService.delete(ids);
             return new Result(true, "删除成功");
@@ -102,13 +115,14 @@ public class SellerController {
 
     /**
      * 查询+分页
-     * @param brand
+     *
+     * @param seller
      * @param page
      * @param rows
      * @return
      */
     @RequestMapping("/search")
-    public PageResult search(@RequestBody TbSeller seller, int page, int rows  ){
+    public PageResult search(@RequestBody TbSeller seller, int page, int rows) {
         return sellerService.findPage(seller, page, rows);
     }
 
