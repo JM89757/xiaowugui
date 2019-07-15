3b3fa3ac34a6c8af3edc7d9fd38a2afea5ce620d
JM89757 <sonymiwu@foxmail.com>
2019-07-15 21:41:45 +0800

    brand project demo1 day11 2019年7月15日21:41:40

---
+++ pinyougou_root/freemarker/src/main/java/cn/itcast/FreeMarker.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+
+import freemarker.template.Configuration;
+
+public class FreeMarker {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration(Configuration.getVersion());
+    }
+}
