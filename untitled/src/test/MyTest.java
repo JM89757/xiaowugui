d5aa3b7093f55eee6f12790ffc4159ed7aad73ca
JM89757 <sonymiwu@foxmail.com>
2019-08-24 13:08:13 +0800

    1111111111111

--- untitled/src/test/MyTest.java
+++ untitled/src/test/MyTest.java
@@ -2,6 +2,6 @@ package test;
 
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("111111111111111");
+        System.out.println("11111111111");
     }
 }
