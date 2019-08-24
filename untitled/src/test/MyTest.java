0c34cea3a9690bb04165522d4acfa4ebd6916c2a
JM89757 <sonymiwu@foxmail.com>
2019-08-24 14:11:29 +0800

    2222222222

--- untitled/src/test/MyTest.java
+++ untitled/src/test/MyTest.java
@@ -3,5 +3,6 @@ package test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("11111111111111");
+        System.out.println("22222222222222");
     }
 }
