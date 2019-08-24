400eb1deaa3e6c2a20d3f14997ba07b98922cd12
JM89757 <sonymiwu@foxmail.com>
2019-08-24 11:53:08 +0800

    pt

--- untitled/untitled/src/com/test/MyTest.java
+++ untitled/untitled/src/com/test/MyTest.java
@@ -3,5 +3,6 @@ package com.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("I  like apple  very much !!!");
+        System.out.println("I like orange too!!!");
     }
 }
